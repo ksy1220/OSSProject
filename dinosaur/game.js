cf5df2cf11dbecfc5866226c5b462c0f38e621cf
ksy1220 <tjdusalsdn@kakao.com>
2022-05-20 07:38:42 +0900

    공룡게임 최종

--- dinosaur/game.js
+++ dinosaur/game.js
@@ -1,16 +1,18 @@
 let config = {
     type: Phaser.AUTO,
+    parent: 'game',
+    autoCenter: true,
     width: 1000,
     height: 400,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 1500 },
-            debug: true
+            debug: false,
         }
     },
     backgroundColor: 0xeeeeee,
-    scene: [MainScene, ScoreScene]
+    scene: [MainScene]
 };
 
 let game = new Phaser.Game(config);
